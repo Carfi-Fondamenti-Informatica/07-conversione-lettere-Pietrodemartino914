f812d33a68863c06860b7ac25b7f734387d31ae1
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-12-06 20:10:38 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,20 +1,15 @@
-#include"lib.h"
+#include "lib.h"
 
-bool lettera (char a){
+
+bool lettera (char &a){
     if ((((int)a>65)and((int)a<90))or(((int)a>97)and((int)a<122))){
+        if ((int)a<=90){
+            a+=32;
+        }  else {
+            a-=32;
+        }
         return true;
-}
-}
-
-
-char converter(char a ){
-    char c =1;
-    if ((int)a<=90){
-        c=(int)a+32;
-        return c;
-
-    }  else {
-        c=(int)a-32;
-        return c;
+    }else {
+        return false;
     }
 }
