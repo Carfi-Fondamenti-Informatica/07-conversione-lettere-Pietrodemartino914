a4406842cf9d4053ac6d643395da7f2fb700f16c
Pietrodemartino914 <115526682+Pietrodemartino914@users.noreply.github.com>
2022-12-06 20:23:04 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,8 +1,9 @@
+
 #include "lib.h"
 
 
 bool lettera (char &a){
-    if ((((int)a>65)and((int)a<90))or(((int)a>97)and((int)a<122))){
+    if ((((int)a>=65)and((int)a<=90))or(((int)a>=97)and((int)a<=122))){
         if ((int)a<=90){
             a+=32;
         }  else {
